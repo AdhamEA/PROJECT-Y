f08d6233503a087c8f60d0ce4fdf0bd289271cd9
AdhamEA <98425353+AdhamEA@users.noreply.github.com>
2022-06-18 02:35:10 +0100

    Update sketch_jan22a.ino

--- sketch_jan22a.ino
+++ sketch_jan22a.ino
@@ -1,38 +1,127 @@
+#include <LiquidCrystal.h>
+#include <Servo.h>
 
-int value= 0;
-int led= 12;
-int led1= 11;
-// the setup function runs once when you press reset or power the board
-void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(led, OUTPUT);
-  pinMode(led1,OUTPUT);
-  Serial.begin(9600);
-  Serial.println("conection established ...");
+LiquidCrystal lcd(5,6,8,9,10,11);
+
+
+#define pinservo 12;
+#define sensorPin  A1
+
+
+
+ Servo servo;
+
+int redled2 = 13;  
+int redled = 2;
+int led2 = 1;
+int greenled = 7;
+int buzzer = 4;
+int sensor = A0;
+int sensorThresh =500;
+int tempSensor =A1;
+float temp;
+int Mcc = 3;
+
+
+void setup()
+{
+  servo.attach(12);
+pinMode(1, OUTPUT);
+pinMode(2,OUTPUT);
+pinMode(7, OUTPUT);
+pinMode(13, OUTPUT);
+pinMode(3,OUTPUT);
+pinMode(buzzer,OUTPUT);
+pinMode(sensor,INPUT);
+pinMode(tempSensor,INPUT);
+pinMode(12,INPUT);
+
+Serial.begin(9600);
+lcd.begin(16,2);
+
 }
 
-// the loop function runs over and over again forever
-void loop() {
-  while(Serial.available())
+void loop()
+{
+  temp = analogRead(A1)*0.004882814;
+  temp=(temp-0.5)*100;
+  int gaz = analogRead(sensor);
+  lcd.setCursor(0,0);
+ lcd.print("Tmp= ");
+  lcd.setCursor(4,0);
+  lcd.print(temp);
+  lcd.setCursor(9, 0);
+ lcd.print("Gaz= ");
+  lcd.setCursor(13,0);
+  lcd.print(gaz);
+delay(1000);
+  lcd.clear();
+  
+  if(gaz>sensorThresh && temp <= 45)
   {
-    value = Serial.read();
-    Serial.println(value);
-    }
-    if (value=='1'){
-  digitalWrite(led, HIGH);}
-    else if (value=='0'){
-      digitalWrite(led, LOW);
-      
-      }
-       else if (value=='2'){
-      digitalWrite(led1, HIGH);}
-      else if (value=='3'){
-      digitalWrite(led1, LOW);}
-     
-    
+    servo.write(-180);
+ digitalWrite(7,LOW);
+ digitalWrite(redled,HIGH);
+ digitalWrite(13,LOW);
+ digitalWrite(1,HIGH);
+ analogWrite(3,255);
+ tone(buzzer,800,1000);
+ lcd.setCursor(0,1);
+ lcd.print("alert Gas leak ");
+ delay(1000);
+ lcd.clear();
+
+  }
+    if(gaz<sensorThresh && temp <= 45)
+
+  {
+    servo.write(180);
+
+
+    digitalWrite(1,HIGH);
+    digitalWrite(13,LOW);
+    digitalWrite(7,HIGH);
+    digitalWrite(2,LOW);
+    analogWrite(3,0);
+
+    noTone(buzzer);
+    lcd.clear();
+    lcd.setCursor(0,1);
+    lcd.print("SAFE");
+    delay(500);
+    lcd.setCursor(0,1);
+    lcd.print("ALL CLEAR");
+    delay(1000);
+    lcd.clear();
+
+  }  
+     if (temp >= 45 && gaz<sensorThresh) {
+
+  digitalWrite(13, HIGH);
+  digitalWrite(2,LOW);
+  digitalWrite(7,HIGH);
+  digitalWrite(1,LOW);
+  analogWrite(3,255);
+  tone(buzzer,800,1000);
+  lcd.setCursor(0,1);
+  lcd.print("fire detected");
+  delay(1000);
+  lcd.clear();
+
+  }
+  if (temp >= 45 && gaz>sensorThresh) {
+
+ digitalWrite(13, HIGH);
+ digitalWrite(2,HIGH);
+ digitalWrite(7,LOW);
+ digitalWrite(1,LOW);
+ analogWrite(3,255);
+ tone(buzzer,800,1000);
+ lcd.setCursor(0,1);
+ lcd.print("Fire & Gaz leak");
+ delay(1000);
+ lcd.clear();
+  }
+      delay(900);
+
 }
-    
-
-
-  // turn the LED on (HIGH is the voltage level)
-                        // wait for a second
